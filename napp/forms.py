ab40b1461941bfacc9d4cdafa5d559d83c9aa8a6
Gagan Hirebidri <gagan.aimit@gmail.com>
2020-06-04 12:38:26 +0530

    first commit

--- napp/forms.py
+++ napp/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserRegistration(forms.ModelForm):
-    password=forms.forms.CharField( max_length=15, required=True,Widget=forms.PasswordInput)
+    password=forms.CharField( max_length=15, required=True,Widget=forms.PasswordInput),
     class Meta:
         model=User
-        fields=('first_name','last_name','user_name','email','password') 
+        fields=('first_name','last_name','username','email','password')
--- napp/views.py
+++ napp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from napp import forms
 
 # Create your views here.
+def home(request):
+    return render(request,'napp/home.html')
+
+def register(request):
+    if request.method=="POST":
+        password=request.POST.get('password')
+        userform=forms.UserRegistration(request.POST)
+        if userform.is_valid():
+            user=userform.save(commit=False)
+            user.set_password(password)
+            user.save()
+    form=forms.UserRegistration()
+    return render(request,'napp/register.html',{'form':form})
